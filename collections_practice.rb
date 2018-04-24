0b0646bcc755ea85f65381bfee983cf2801db455
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-24 18:59:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,6 +16,6 @@ def swap_elements(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
-  array.insert(destination_index, array[index]).pop
+  array.insert(destination_index, array[index]).
   return array
 end
